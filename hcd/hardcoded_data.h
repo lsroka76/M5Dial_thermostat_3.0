badbda5f5bb25f40bf66750892ef06f2abe2df8f
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2024-05-30 22:22:44 +0200

    Create hardcoded_data.h

---
+++ hcd/hardcoded_data.h
@@ -0,0 +1,21 @@
+#define SUPLA_WIFI_SSID wifi_ssid
+#define SUPLA_WIFI_PASS wifi_pass
+#define SUPLA_SVR       supla_svr
+#define SUPLA_EMAIL     supla_email
+
+const static char USER_GUID[SUPLA_GUID_SIZE] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+const static char USER_AUTH_KEY[SUPLA_AUTHKEY_SIZE] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+
+#define HCD_SENSORS 9
+
+const static char *xiaomiBleDeviceMacsTEMP[][2] PROGMEM = {  
+{"00:00:00:00:00:00","lokalizacja 1"},
+{"00:00:00:00:00:00","lokalizacja 2"},
+{"00:00:00:00:00:00","lokalizacja 3"},
+{"00:00:00:00:00:00","lokalizacja 4"},
+{"00:00:00:00:00:00","lokalizacja 5"},
+{"00:00:00:00:00:00","lokalizacja 6"},
+{"00:00:00:00:00:00","lokalizacja 7"},
+{"00:00:00:00:00:00","lokalizacja 8"},
+{"00:00:00:00:00:00","lokalizacja 9"}
+};
